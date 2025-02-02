b5219e30ceed7f89da914663687255e6544b43b1
Jose Arniel Pama <pamajosearniel@gmail.com>
2025-02-03 01:06:48 +0800

    remove placeholder images; create elements for links and photos and add to DOM

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
+var photosArray = [];
+const imageContainer = document.getElementById("image-container");
+
 // Unsplash API
 const count = 10;
 const apiKey = "mkbunIFGU8cWsJKgqwLuK1XdstUK6lvMVb8quOmojJw";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create elements for links and photos, add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    // Create <img> for photo
+    const img = document.createElement("img");
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Put <img> inside <a>, then put both inside imageContainer element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 // Get photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log("data: ", data);
+    photosArray = await response.json();
+    console.log("photosArray: ", photosArray);
+    displayPhotos();
   } catch (error) {
     // catch error here
   }
